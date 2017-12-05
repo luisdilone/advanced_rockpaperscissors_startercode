33477c2f05116921cfd5281370564a0a2eecc7a7
Luis Angel Dilone <ldilone@aclanyc.com>
2017-12-05 23:14:07 +0000

    write input field functions

--- js/script.js
+++ js/script.js
@@ -2,9 +2,37 @@
         // Fork and clone the repo and set up your project workspace and link files
         // Write HTML for your project
 //Class 2:  
-        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
+        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.
         // Practice using console.log() and debugger to debug your code
         // Create your document ready function. 
+        $(document).ready(function(){
+                function process (){
+                        
+                }
+                
+                $("#form").click(function(){
+                        userName = $("#name").val();
+                        // $("#outcome").append("<p>",userName, " Wins","<p>")
+                });
+                $("#rock").click(function (){
+                        userChoice = "rock"
+                        process();
+                })
+                 $("#paper").click(function (){
+                        userChoice = "paper"
+                        process();
+                })
+                 $("#scissors").click(function (){
+                        userChoice = "scissors"
+                        process();
+                })
+                
+                
+                 
+                
+        });
+                
+        
         // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
         // Display the user choice to the output screen
 //Class 3:  
@@ -13,18 +41,44 @@
         // Compare the user choice and the computer choice to determine who won. 
         // Display the user winner to the output screen 
         // Clear the input box for the next choice
-
+        
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
 
-
+var userChoice = "";
+var userName = "Mystery Person";
+var computerChoice = "paper";
+var winner = "";
+var choices = ["rock","paper","scissors"]
 
 //FUNCTIONS
 
-
+function process (){
+                        
+                }
 
 // DOCUMENT READY FUNCTION
 
+$(document).ready(function(){
+              
+                
+        $("#form").click(function(){
+                userName = $("#name").val();
+                // $("#outcome").append("<p>",userName, " Wins","<p>")
+                });
+                $("#rock").click(function (){
+                        userChoice = "rock"
+                        process();
+                })
+                 $("#paper").click(function (){
+                        userChoice = "paper"
+                        process();
+                })
+                 $("#scissors").click(function (){
+                        userChoice = "scissors"
+                        process();
+                })
+        });
